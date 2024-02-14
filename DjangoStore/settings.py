adf7941b83bdc4397779d4f0a8b2d7dd55e31b85
Hossein <hossienbehzadi1381@gmail.com>
2024-02-14 23:17:28 +0330

    adding tamplate and sale price in models

--- DjangoStore/settings.py
+++ DjangoStore/settings.py
@@ -106,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIES = ['static/']
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URl = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- store/models.py
+++ store/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
@@ -27,9 +30,22 @@ class Product(models.Model):
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
+    in_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
-     
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=150, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.product
