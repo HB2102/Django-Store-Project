9b76bfd433530e7485f33639dacd92c10ba7a498
Hossein <hossienbehzadi1381@gmail.com>
2024-02-15 00:46:38 +0330

    adding login page and views

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -9,3 +11,29 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You logged in successfully.')
+            return redirect('home')
+
+        else:
+            messages.success(request, 'There was a problem please try again')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You successfully logged out.')
+    return redirect('home')
+
