99f5c831dc4cdf3ac6fc32c0af925acf84282da0
Hossein <hossienbehzadi1381@gmail.com>
2024-02-15 12:54:38 +0330

    adding cart session and add to cart function

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 
 def cart_summary(request):
@@ -6,7 +9,17 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == "post":
+        product_id = int(request.POST.get('product_id'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product)
+
+        response = JsonResponse({'product name': product.name})
+        return response
 
 
 def cart_delete(request):
