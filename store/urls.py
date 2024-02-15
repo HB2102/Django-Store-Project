01672d1b7c7195276a56b6b6c7da3e58ea71181c
Hossein <hossienbehzadi1381@gmail.com>
2024-02-15 16:52:21 +0330

    adding user password edit page and view

--- store/urls.py
+++ store/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('category/<str:cat>', views.category, name='category'),
     path('category_summary/', views.category_summary, name='category_summary'),
     path('update_user/', views.update_user, name='update_user'),
+    path('update_password/', views.update_password, name='update_password'),
 ]
 
 # if settings.DEBUG:
--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from django import forms
 
 
@@ -106,4 +106,31 @@ def update_user(request):
     else:
         messages.error(request, 'You must be logged in to access this page.')
         return redirect('home')
+
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+
+        if request.method == "POST":
+            form = ChangePasswordForm(current_user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Your Password has been Updated.')
+                login(request, current_user)
+                return redirect('update_user')
+
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, 'update_password.html', {'form': form})
+
+    else:
+        messages.error(request, 'You must be logged in to access this page.')
+        return redirect('home')
+
 
