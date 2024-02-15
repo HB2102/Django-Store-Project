150dad77d9811259c59829f67f71fe1234861107
Hossein <hossienbehzadi1381@gmail.com>
2024-02-15 17:33:42 +0330

    extend user profile

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
 
 
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 import datetime
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=150, blank=True)
+    address2 = models.CharField(max_length=150, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    country = models.CharField(max_length=150, blank=True)
+    zipcode = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
 
 
 class Category(models.Model):
@@ -45,7 +70,6 @@ class Order(models.Model):
     phone = models.CharField(max_length=20, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanField(default=False)
-
 
     def __str__(self):
         return self.product
